32a1df78eb41e311a815854383132638905be40f
avital477 <33714153+avital477@users.noreply.github.com>
2018-05-27 21:15:27 +0300

    Add files via upload

--- Board.cpp
+++ Board.cpp
@@ -18,7 +18,6 @@ Board::Board(const Board& b){
 }
 
 
-
 void Board::create(int newsize) {
 	size = newsize;
 	matrix = new Place*[size];
@@ -68,5 +67,18 @@ Board& Board::operator=(const Board& b) {
 	}
 	return *this;
 }
+
+istream& operator >>(istream& is, Board& b){
+     int size;
+     is>> "enter size: " >>size;
+	 for(int i=0; i<size;i++){
+		 for(int j=0; j<size;j++){
+			 char ch;
+			 is>> ch;
+		 }
+	 }
+     
+     return is;
+ }
 
 
